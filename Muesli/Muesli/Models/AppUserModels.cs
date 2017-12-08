53abf41c9394343d8dbf80ac895193488f1ab833
StartedFromTheBottom <giintas>
2017-12-08 11:08:14 +0100

    test2

---
+++ Muesli/Muesli/Models/AppUserModels.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Muesli.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
+
