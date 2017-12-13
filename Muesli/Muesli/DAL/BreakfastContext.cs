4a521f15d4e63c1ae270707de13e4df7fe3c945a
StartedFromTheBottom <giintas>
2017-12-13 07:59:19 +0100

    removed the identity login

--- Muesli/Muesli/DAL/BreakfastContext.cs
+++ Muesli/Muesli/DAL/BreakfastContext.cs
@@ -22,39 +22,10 @@ namespace Muesli.DAL
         {
 
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
-            //modelBuilder.Entity<BreakfastContext>();
         }
     }
     // breakfast end
 
-    public class AppIdentityDbContext : IdentityDbContext<AppUser>
-    {
-        public AppIdentityDbContext() : base("BreakfastContext") { }
-        // public AppIdentityDbContext() : base("name=IdentityDb") { }
-
-        static AppIdentityDbContext()
-        {
-            Database.SetInitializer<AppIdentityDbContext>(new IdentityDbInit());
-        }
-
-        public static AppIdentityDbContext Create()
-        {
-            return new AppIdentityDbContext();
-        }
-    }
-
-    public class IdentityDbInit : DropCreateDatabaseIfModelChanges<AppIdentityDbContext>
-    {
-        protected override void Seed(AppIdentityDbContext context)
-        {
-            PerformInitialSetup(context);
-            base.Seed(context);
-        }
-        public void PerformInitialSetup(AppIdentityDbContext context)
-        {
-            // настройки конфигурации контекста будут указываться здесь
-        }
-    }
 
 
 }
